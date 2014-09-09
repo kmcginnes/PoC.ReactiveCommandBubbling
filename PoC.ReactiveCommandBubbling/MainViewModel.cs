5e3e6ac5895e75a19c87507a7c704f41d28f7f30
Kris McGinnes <kmcginnes@me.com>
2014-09-08 21:39:46 -0500

    Add view model with list of characters

---
+++ PoC.ReactiveCommandBubbling/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ReactiveUI;
+
+namespace PoC.ReactiveCommandBubbling
+{
+    public class MainViewModel : ReactiveObject
+    {
+        public MainViewModel()
+        {
+            People = new ReactiveList<PersonViewModel>();
+            People.Add(new PersonViewModel("Jerry", "Seinfeld"));
+            People.Add(new PersonViewModel("George", "Costanza"));
+        }
+
+        public ReactiveList<PersonViewModel> People { get; private set; }
+    }
+
+    public class PersonViewModel : ReactiveObject
+    {
+        public PersonViewModel(string first, string last)
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
